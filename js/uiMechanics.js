092245b43e93999f0e0738caccb103efa33eb778
Lucas <lucasdesenna@gmail.com>
2012-06-10 14:35:26 -0300

    Stats in Weapons

--- js/uiMechanics.js
+++ js/uiMechanics.js
@@ -1,5 +1,6 @@
 function createStat(target) {
-	$(target).parent().append("<li class='equipStat'><select><option>Str</option><option>Dex</option><option>Int</option><option>Vit</option></select><input type ='number' min='1' placeholder='0' /><button type='button' onclick='removeParent(this);'>x</button></li>");
+	$(target).parent().append("<li class='equipStat'><select><option>Str</option><option>Dex</option><option>Int</option><option>Vit</option></select><input type ='number' min='1' value='1'/><button type='button' onclick='removeParent(this);'>x</button></li>");
+	updateChar();
 }
 
 function removeParent(target) {
