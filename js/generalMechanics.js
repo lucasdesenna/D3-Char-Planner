522bea38b527d596b8559a43e30407427bd25130
Lucas <lucasdesenna@gmail.com>
2012-06-28 00:39:45 -0300

    Att and Buff retrieval

--- js/generalMechanics.js
+++ js/generalMechanics.js
@@ -1,28 +1,14 @@
-var char = new Object;
+var char = {};
 	char["level"] = 60;
-    char["name"];
-	char["class"];
-/*Main Stats*/
-	char["str"];
-	char["dex"];
-	char["int"];
-	char["vit"];
-	char["armor"];
-	char["avgRes"];
-	char["hp"];
 /*Secondary Stats*/
 	char["avgDPS"];
 	char["armorDmgRed"];
 	char["avgResDmgRed"];
 	char["ehp"];
 /*Weapon*/
-var weapon = new Object;
-	weapon["type"];
-	weapon["atkSpd"];
-	weapon["maxDmg"];
-	weapon["minDmg"];
+var weapon = {};
 /*Equips*/
-var equips = new Object;
+var equips = {};
 /*-----------------------------------*/
 
 function updateChar() {
@@ -38,7 +24,6 @@ function updateChar() {
 	getTotalArmor();
 	getTotalAtt();
 	getHP();
-	getDPS();
 	updateStats();
 }
 
@@ -51,17 +36,28 @@ function getCharData() {
 }
 
 function getWeaponData() {
-	weaponType = $("select#weaponType").html();
-	weaponAtkSpd = parseFloat($("select#weaponType option:selected").val());
-	if($("#weaponMaxDmg").val()) { weaponMaxDmg = parseInt($("#weaponMaxDmg").val()); }
-	else { weaponMaxDmg = 0; }
-	if($("#weaponMinDmg").val()) { weaponMinDmg = parseInt($("#weaponMinDmg").val()); }
-	else { weaponMinDmg = 0; }
+	weapon["type"] = $("select#weaponType").html();
+	weapon["atkSpd"] = parseFloat($("select#weaponType option:selected").val());
+	if($("#weaponMaxDmg").val()) { weapon["maxDmg"] = parseInt($("#weaponMaxDmg").val()); }
+	else { weapon["maxDmg"] = 0; }
+	if($("#weaponMinDmg").val()) { weapon["minDmg"] = parseInt($("#weaponMinDmg").val()); }
+	else {weapon["minDmg"] = 0; }
+	var attList = $("tr#mainWeapon").find("li.equipAtt select option:selected");
+		var attValueList = $("tr#mainWeapon").find("li.equipAtt input");
+		for( var j = 0; j < attList.length; j++ ) {
+			weapon[$(attList[j]).val()] = parseInt($(attValueList[j]).val());
+		}
+		
+	var buffList = $("tr#mainWeapon").find("li.equipBuff select option:selected");
+	var buffValueList = $("tr#mainWeapon").find("li.equipBuff input");
+	for( var k = 0; k < buffList.length; k++ ) {
+		equips[$(buffList[k]).val()] = parseInt($(buffValueList[k]).val());
+	}
 }
 
 function getEquipData() {
-	var equipList = $(document).find("tr.equip");
-	for( var i in equipList ) {
+	var equipList = $.find("tr.equip");
+	for( var i = 0; i < equipList.length; i++ ) {
 		var itemName = $(equipList[i]).find("th.itemCol");
 		itemName = $(itemName[0]).html();
 		if(!equips[itemName]) { equips[itemName] = new Object; }
@@ -70,13 +66,13 @@ function getEquipData() {
 		
 		var attList = $(equipList[i]).find("li.equipAtt select option:selected");
 		var attValueList = $(equipList[i]).find("li.equipAtt input");
-		for( var j in attList) {
+		for( var j = 0; j < attList.length; j++ ) {
 			equips[itemName][$(attList[j]).val()] = parseInt($(attValueList[j]).val());
 		}
 		
-		var buffList = $(equipList[i]).find("li.equipAtt select option:selected");
-		var buffValueList = $(equipList[i]).find("li.equipAtt input");
-		for( var k in buffList) {
+		var buffList = $(equipList[i]).find("li.equipBuff select option:selected");
+		var buffValueList = $(equipList[i]).find("li.equipBuff input");
+		for( var k = 0; k < buffList.length; k++ ) {
 			equips[itemName][$(buffList[k]).val()] = parseInt($(buffValueList[k]).val());
 		}
 	}
@@ -94,7 +90,7 @@ function updateStats() {
 function getEquipArmor() {
 	var equipArmor = 0;
 	var equipList = $(".equipArmor");
-	for( var i in equiList ) {
+	for( var i = 0; i < equipList.length; i++ ) {
 		if (parseInt($(equipList[i]).val()) > 0) {
 			equipArmor += parseInt($(equipList[i]).val());
 		}
@@ -104,16 +100,20 @@ function getEquipArmor() {
 }
 
 function getTotalAtt() {
-	var equipList = [];
-	for( var i in equips) {
-		if( equips.hasOwnProperty(i) ) { equipList.push(i); }
-	}
+	if (weapon["str"]) { char["str"] += weapon["str"]; }
+	if (weapon["dex"]) { char["dex"] += weapon["dex"]; }
+	if (weapon["int"]) { char["int"] += weapon["int"]; }
+	if (weapon["vit"]) { char["vit"] += weapon["vit"]; }
 	
-	for( var j in equipList ) {
-		char["str"] += equipList[j]["str"];
-		char["dex"] += equipList[j]["dex"];
-		char["int"] += equipList[j]["int"];
-		char["vit"] += equipList[j]["vit"];
+	var keyList = new Array;
+	for( var key in equips ) {
+		keyList.push(key);
+	}	
+	for( var i = 0; i < keyList.length; i++ ) {
+		if (equips[keyList[i]]["str"]) { char["str"] += equips[keyList[i]]["str"]; }
+		if (equips[keyList[i]]["dex"]) { char["dex"] += equips[keyList[i]]["dex"]; }
+		if (equips[keyList[i]]["int"]) { char["int"] += equips[keyList[i]]["int"]; }
+		if (equips[keyList[i]]["vit"]) { char["vit"] += equips[keyList[i]]["vit"]; }
 	}
 }
 
